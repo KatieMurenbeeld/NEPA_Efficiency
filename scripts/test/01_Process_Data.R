57846c40015f653ce4ad6b513e72335a17e40709
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-09-05 13:46:54 -0600

    survival analysis script, data processing script, and processed data as csv

---
+++ scripts/test/01_Process_Data.R
@@ -0,0 +1,38 @@
+## Process and save the PALS data for use in a survival analysis
+
+library(tidyverse) # used to manipulate data frames
+
+## Read in the PALS data and forests shapefile
+pals <- read.csv("data/original/pals_ongoing_projects_11-2022.csv", sep = ";")
+
+# PALS: select desired columns/variables
+pals_df <- select(pals, c(REGION_ID, FOREST_ID, PROJECT.NUMBER, PROJECT.NAME, PROJECT.STATUS,
+                          PROJECT.CREATED, INITIATION.DATE, DECISION.SIGNED, DECISION.TYPE,
+                          ELAPSED.DAYS, DECISION_LEVEL, ongoing))
+
+# PALS: create a binary column to serve as the Event Occurring for the survival analysis. 
+# Status of Completed = 1, every other status = 0. This will make In Progress and Cancelled projects = 0.
+pals_df$EVENT <- ifelse(pals_df$PROJECT.STATUS == "Complete", 1, 0)
+
+# PALS: replace NA with 0 for ongoing
+pals_df$ongoing[is.na(pals_df$ongoing)] <- 0
+
+# PALS: make FOREST_ID a factor
+pals_df$FOREST_ID <- as.factor(pals_df$FOREST_ID)
+
+# PALS: filter by year initiated >2009-01-01 and status = Complete
+## First need to convert character strings to datetime or calendar time objects
+pals_df$PROJECT.CREATED <- as.Date(pals_df$PROJECT.CREATED, format = "%m/%d/%Y")
+pals_df$INITIATION.DATE <- as.Date(pals_df$INITIATION.DATE, format = "%m/%d/%Y")
+pals_df$DECISION.SIGNED <- as.Date(pals_df$DECISION.SIGNED, format = "%m/%d/%Y")
+
+## Then filter by year NEPA was initiated and (&) status = complete
+## Also remove Test Project TEST and project 35233
+pals2009_df <- pals_df %>%
+  filter(INITIATION.DATE > "2009-01-01" & PROJECT.STATUS == "Complete") %>%
+  filter(PROJECT.NAME != "Test Project TEST") %>%
+  filter(PROJECT.NUMBER != 35233) %>%
+  filter(ongoing == 0)
+
+## save pals2009_df as a .csv file to "data/processed/"
+write_csv(pals2009_df, "data/processed/pals_2009.csv")
---
+++ scripts/test/02_Survival_Analysis.R
@@ -0,0 +1,15 @@
+
+
+library(tidyverse)
+library(survminer)
+library(survival)
+
+df <- read_csv("data/processed/pals_2009.csv")
+# Note to self: may need to save REGION_ID and FOREST_ID as characters not numeric in 01_*.R
+
+fit_assess_all <- survfit(Surv(ELAPSED.DAYS, EVENT) ~ 1, data = df)
+fit_assess_summary <- fit_assess_all %>% tidy()
+
+fit_assess_forest <- survfit(Surv(ELAPSED.DAYS, EVENT) ~ FOREST_ID, data = df)
+fit_forest_summary <- fit_assess_forest %>% tidy()
+

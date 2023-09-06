5395c099278c716638cc9af5c322d6fca6ca7504
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-09-05 23:44:42 -0600

    Renamed script. Began test plotting maps

--- 03_Combine_Data_Plot_Map.R
+++ 03_Combine_Data_Plot_Map.R
@@ -1,6 +1,9 @@
 ## Combine the survival probability by forest with the USFS Nation Forest Admin boundaries 
 
 library(sf)
+library(ggplot2)
+library(ggmap)
+library(tigris)
 
 # Load the files to combine
 forest_surv <- read_csv("data/processed/forest_surv.csv")
@@ -9,3 +12,16 @@ forest_boundary <- select(forest_boundary, -c(ADMINFORES, REGION, FORESTNUMB))
 
 joined_df <- left_join(forest_boundary, forest_surv,
                        by = c("FORESTORGC" = "FOREST_ID"))
+
+
+#st_write(joined_df, "data/processed/surv_prob.shp") # I think this file will be too big for github. 
+# May have to have this script combine the data and make a map.
+
+us_states <- states(cb = TRUE) %>%
+  shift_geometry()
+
+ggplot() +
+  geom_sf(data = us_states, fill = NA, color = "black", size = 0.1)
+
+ggplot() +
+  geom_sf(data = joined_df, aes(fill = SURV_1YR))

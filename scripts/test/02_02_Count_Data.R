60cae006f1ea379c1d96e00005b794f2a53dfdee
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-09-27 15:08:18 -0600

    creating joined table

--- scripts/test/02_02_Count_Data.R
+++ scripts/test/02_02_Count_Data.R
@@ -6,7 +6,7 @@ df <- read_csv("data/processed/pals_2009.csv")
 longer1yr <- df %>%
   filter(LONGER_1YR == 1) %>%
   group_by(FOREST_ID) %>%
-  summarise(total = sum(LONGER_1YR))
+  summarise(longer1 = sum(LONGER_1YR))
 
 less1yr <- df %>%
   filter(LONGER_1YR == 0) %>%
@@ -16,7 +16,7 @@ less1yr <- df %>%
 longer2yr <- df %>%
   filter(LONGER_2YR == 1) %>%
   group_by(FOREST_ID) %>%
-  summarise(total = sum(LONGER_2YR))
+  summarise(longer2 = sum(LONGER_2YR))
 
 less2yr <- df %>%
   filter(LONGER_2YR == 0) %>%
@@ -27,3 +27,9 @@ n_proj <- df %>% count(FOREST_ID)
 
 reg_for <- df %>% group_by(FOREST_ID) %>%
   summarise(region = mean(REGION_ID))
+
+new_df <- left_join(reg_for, n_proj, by = "FOREST_ID") %>%
+  left_join(longer1yr, by = "FOREST_ID") %>% 
+  left_join(less1yr, by = "FOREST_ID") %>%
+  left_join(longer2yr, by = "FOREST_ID") %>%
+  left_join(less2yr, by = "FOREST_ID")

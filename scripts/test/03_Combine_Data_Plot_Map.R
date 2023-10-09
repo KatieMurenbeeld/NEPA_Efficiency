098d0da503246bfbe474b4aeac2a4fb86ebc164a
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-10-08 22:04:35 -0600

    create dataframe with counts of projects that have >1 or >2yr to complete NEPA

--- scripts/test/03_Combine_Data_Plot_Map.R
+++ scripts/test/03_Combine_Data_Plot_Map.R
@@ -9,23 +9,30 @@ library(tigris)
 # Load the files to combine
 forest_surv <- read_csv("data/processed/forest_surv.csv")
 region_surv <- read_csv("data/processed/region_surv.csv")
+count <- read_csv("data/processed/long_nepa_count.csv")
+count$FOREST_ID <- as.character(count$FOREST_ID)
+count$FOREST_ID <- paste0("0", count$FOREST_ID)
 forest_boundary <- st_read('data/original/S_USA.AdministrativeForest.shp')
 forest_boundary <- select(forest_boundary, -c(ADMINFORES, REGION, FORESTNUMB))
 region_boundary <- st_read("data/original/S_USA.AdministrativeRegion.shp")
 region_boundary <- select(region_boundary, -c(ADMINREGIO))
 
-forest_df <- left_join(forest_boundary, forest_surv,
+forest_surv_df <- left_join(forest_boundary, forest_surv,
                        by = c("FORESTORGC" = "FOREST_ID")) %>%
   filter(FORESTORGC != "1004" & FORESTORGC != "1005" & FORESTORGC != "0816")
 
-region_df <- left_join(region_boundary, region_surv,
+region_surv_df <- left_join(region_boundary, region_surv,
                        by = c("REGION" = "REGION_ID")) %>%
   filter(REGION != "10")
+
+count_df <- left_join(forest_boundary, count,
+                      by = c("FORESTORGC" = "FOREST_ID")) %>%
+  filter(FORESTORGC != "1004" & FORESTORGC != "1005" & FORESTORGC != "0816")
+
 
 
 #st_write(joined_df, "data/processed/surv_prob.shp") # I think this file will be too big for github. 
 # May have to have this script combine the data and make a map. 
-
 
 us_states <- states(cb = TRUE) %>%
   filter(GEOID < "60") %>%
@@ -38,10 +45,16 @@ ggplot() +
 
 nf_surv1yr <- ggplot() +
   geom_sf(data = us_states, fill = NA, color = "black", size = 0.1) +
-  geom_sf(data = forest_df, aes(fill = SURV_1YR), size = 0.05) + 
+  geom_sf(data = forest_surv_df, aes(fill = SURV_1YR), size = 0.05) + 
   scale_fill_gradient(low = "white", high = "forestgreen")
 ggsave("nf_surv1yr.png", nf_surv1yr, width = 12, height = 12, dpi = 300)
 dev.off()
+
+nf_count1yr <- ggplot() +
+  geom_sf(data = us_states, fill = NA, color = "black", size = 0.1) +
+  geom_sf(data = count_df, aes(fill = SURV_1YR), size = 0.05) + 
+  scale_fill_gradient(low = "white", high = "forestgreen")
+ggsave("nf_surv1yr.png", nf_surv1yr, width = 12, height = 12, dpi = 300)
 
 nf_surv2yr <- ggplot() +
   geom_sf(data = us_states, fill = NA, color = "black", size = 0.1) +

9b3bb06895dc729767fab417d0d504c209d78679
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-11-13 13:40:23 -0700

    code to load government data (forest service and fws)

--- scripts/test/00_ca_load_data.R
+++ scripts/test/00_ca_load_data.R
@@ -1,7 +1,7 @@
 # 
+library(sf)
 
-
-## Use tempfile() and unzip() to load the US 
+## Use tempfile() and unzip() to load the US Forest Service National Forest and Regional boundaries
 tmp <- tempfile()
 fs.nf.url <- "https://data.fs.usda.gov/geodata/edw/edw_resources/shp/S_USA.AdministrativeForest.zip"
 download.file(fs.nf.url, tmp)
@@ -9,8 +9,22 @@ tmp2 <- tempfile()
 unzip(zipfile=tmp, exdir = tmp2 )
 fs.nf.bdry <- read_sf(tmp2)
 
+tmp <- tempfile()
 fs.rg.url <- "https://data.fs.usda.gov/geodata/edw/edw_resources/shp/S_USA.AdministrativeRegion.zip"
 download.file(fs.rg.url, tmp)
 tmp2 <- tempfile()
 unzip(zipfile=tmp, exdir = tmp2 )
 fs.rg.bdry <- read_sf(tmp2)
+
+## Use tempfile() and unzip() to load the Threatened and Endangered Species Critical Habitats
+
+tmp <- tempfile()
+fws.te.url <- "https://ecos.fws.gov/docs/crithab/crithab_all/crithab_all_layers.zip"
+download.file(fws.te.url, tmp)
+tmp2 <- tempfile()
+unzip(zipfile=tmp, exdir = tmp2 )
+fws.te.bdry <- read_sf(tmp2)
+
+
+
+

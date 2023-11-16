69e2bbb658ae62cac391f4b38a7322c517f10a3c
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-11-16 12:53:52 -0700

    code for new maps with FS forest boundaries and a different variables

--- scripts/test/02_ca_base_maps.R
+++ scripts/test/02_ca_base_maps.R
@@ -18,15 +18,26 @@ landuse_subset <- rast("data/processed/landuse_subset.tif")
 
 ## Map 
 
+### Use st_intersection to "clip" the datasets to the FS Region 1 boundary
+
 fws_subset_rg1 <- st_intersection(fws_subset, fs_reg1)
 fs_subset_rg1 <- st_intersection(fs_subset, fs_reg1)
 cejst_subset_rg1 <- st_intersection(cejst_subset, fs_reg1)
 elect_subset_rg1 <- st_intersection(elect_subset, fs_reg1)
 
-test_map <- ggplot() +
-  #geom_sf(data = elect_subset_rg1, aes(fill = rrlrbn_), color = NA, alpha = 0.3, size = 0.1) +
-  geom_sf(data = cejst_subset_rg1, aes(fill = EALR_PFS), color = NA, alpha = 0.5, size = 0.1) +
-  geom_sf(data = fs_subset_rg1, fill = NA, color = "darkgreen", size = 0.1) + 
-  geom_sf(data = fws_subset_rg1, fill = NA, color = "orange", size = 0.1)
-ggsave("test_map.png", test_map, width = 12, height = 12, dpi = 300)
+### Create test maps with outlines of the Nation Forests in Region 1 and some variables
+nf_crit_hab_map <- ggplot() +
+  geom_sf(data = fs_subset_rg1, fill = "darkgreen", alpha = 0.5, size = 0.1) + 
+  geom_sf(data = fws_subset_rg1, fill = NA, color = "orange", size = 0.2)
+ggsave("nf_crit_hab_map.png", nf_crit_hab_map, width = 12, height = 12, dpi = 300)
+
+nf_rural_pct_map <- ggplot() +
+  geom_sf(data = elect_subset_rg1, aes(fill = rrl_pct), color = NA, alpha = 0.65, size = 0.1) +
+  geom_sf(data = fs_subset_rg1, fill = NA, color = "darkgreen", size = 0.1)
+ggsave("nf_rural_pct_map.png", nf_rural_pct_map, width = 12, height = 12, dpi = 300)
+
+nf_ealr_map <- ggplot() +
+  geom_sf(data = cejst_subset_rg1, aes(fill = EALR_PFS), color = NA, alpha = 0.65, size = 0.1) +
+  geom_sf(data = fs_subset_rg1, fill = NA, color = "darkgreen", size = 0.1)
+ggsave("nf_ealr_map.png", nf_ealr_map, width = 12, height = 12, dpi = 300)
 

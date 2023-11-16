272c7919e7ea1048ff773732dbb84b48b02b86e2
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-11-16 12:58:40 -0700

    Added FS region 1 boundary

--- scripts/test/02_ca_base_maps.R
+++ scripts/test/02_ca_base_maps.R
@@ -28,16 +28,19 @@ elect_subset_rg1 <- st_intersection(elect_subset, fs_reg1)
 ### Create test maps with outlines of the Nation Forests in Region 1 and some variables
 nf_crit_hab_map <- ggplot() +
   geom_sf(data = fs_subset_rg1, fill = "darkgreen", alpha = 0.5, size = 0.1) + 
-  geom_sf(data = fws_subset_rg1, fill = NA, color = "orange", size = 0.2)
+  geom_sf(data = fws_subset_rg1, fill = NA, color = "orange", size = 0.2) +
+  geom_sf(data = fs_reg1, fill = NA, color = "black", size = 0.5)
 ggsave("nf_crit_hab_map.png", nf_crit_hab_map, width = 12, height = 12, dpi = 300)
 
 nf_rural_pct_map <- ggplot() +
   geom_sf(data = elect_subset_rg1, aes(fill = rrl_pct), color = NA, alpha = 0.65, size = 0.1) +
-  geom_sf(data = fs_subset_rg1, fill = NA, color = "darkgreen", size = 0.1)
+  geom_sf(data = fs_subset_rg1, fill = NA, color = "darkgreen", size = 0.1) +
+  geom_sf(data = fs_reg1, fill = NA, color = "black", size = 0.5)
 ggsave("nf_rural_pct_map.png", nf_rural_pct_map, width = 12, height = 12, dpi = 300)
 
 nf_ealr_map <- ggplot() +
   geom_sf(data = cejst_subset_rg1, aes(fill = EALR_PFS), color = NA, alpha = 0.65, size = 0.1) +
-  geom_sf(data = fs_subset_rg1, fill = NA, color = "darkgreen", size = 0.1)
+  geom_sf(data = fs_subset_rg1, fill = NA, color = "darkgreen", size = 0.1) +
+  geom_sf(data = fs_reg1, fill = NA, color = "black", size = 0.5)
 ggsave("nf_ealr_map.png", nf_ealr_map, width = 12, height = 12, dpi = 300)
 

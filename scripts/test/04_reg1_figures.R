453c66a0dc0e5c995d230deb5a30f11edf8b2357
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-12-13 23:16:40 -0700

    plot updates

--- scripts/test/04_reg1_figures.R
+++ scripts/test/04_reg1_figures.R
@@ -37,7 +37,7 @@ vdep.df <- reg1_attri$LABEL %>% as.data.frame(xy = TRUE)
 
 rrl_map <- ggplot() +
   geom_raster(aes(x = rrl.df$x, y = rrl.df$y, fill = rrl.df$rrlrbn_)) +
-  geom_sf(data = fs_subset.proj, fill = "white", color = "black", size = 1.5, alpha = 0.25) +
+  geom_sf(data = fs_subset.proj, fill = NA, color = "black", size = 2) +
   scale_fill_viridis("Rural-Urban Continuum", option = "plasma", alpha = 0.5) +
   labs(title = "Region 1: Rural-Urban Continuum", x = "", y = "") +
   theme_bw() +
@@ -49,7 +49,7 @@ ggsave("rrl_map.png", plot = rrl_map, width = 12, height = 12, dpi = 300)
 
 lcv_map <- ggplot() +
   geom_raster(aes(x = lcv.df$x, y = lcv.df$y, fill = lcv.df$LCVScore)) +
-  geom_sf(data = fs_subset.proj, fill = "white", color = "black", size = 1.5, alpha = 0.25) +
+  geom_sf(data = fs_subset.proj, fill = NA, color = "black", size = 2) +
   scale_fill_viridis("LCV Score", option = "plasma", alpha = 0.5) +
   labs(title = "Region 1: League of Conservation Voters Score", x = "", y = "") +
   theme_bw() +
@@ -61,8 +61,8 @@ ggsave("lcv_map.png", plot = lcv_map, width = 12, height = 12, dpi = 300)
 
 nlcd_map <- ggplot() +
   geom_raster(aes(x = nlcd.df$x, y = nlcd.df$y, fill = nlcd.df$`NLCD Land Cover Class`)) +
-  geom_sf(data = fs_subset.proj, fill = "white", color = "black", size = 1.5, alpha = 0.05) +
-  scale_fill_gradient("Land Cover Class", low = "tan", high = "darkgreen") +
+  geom_sf(data = fs_subset.proj, fill = NA, color = "black", size = 2) +
+  scale_fill_gradient("Land Cover Class", low = "darkgreen", high = "tan") +
   labs(title = "Region 1: National Land Cover Class", x = "", y = "") +
   theme_bw() +
   theme(plot.title = element_text(size=22), 
@@ -73,7 +73,7 @@ ggsave("nlcd_map.png", plot = nlcd_map, width = 12, height = 12, dpi = 300)
 
 vdep_map <- ggplot() +
   geom_raster(aes(x = vdep.df$x, y = vdep.df$y, fill = vdep.df$LABEL)) +
-  geom_sf(data = fs_subset.proj, fill = "white", color = "black", size = 1.5, alpha = 0.05) +
+  geom_sf(data = fs_subset.proj, fill = NA, color = "black", size = 2) +
   scale_fill_gradient("Veg. Depart. Index", low = "white", high = "black") +
   labs(title = " Region 1: Vegetation Departure Index", x = "", y = "") +
   theme_bw() +
@@ -85,7 +85,7 @@ ggsave("vdep_map.png", plot = vdep_map, width = 12, height = 12, dpi = 300)
 
 whp_map <- ggplot() +
   geom_raster(aes(x = whp.df$x, y = whp.df$y, fill = whp.df$WHP_ID)) +
-  geom_sf(data = fs_subset.proj, fill = "white", color = "black", size = 1.5, alpha = 0.05) +
+  geom_sf(data = fs_subset.proj, fill = NA, color = "white", size = 2) +
   scale_fill_viridis("Wildfire Haz. Potential", option = "inferno", alpha = 0.5) +
   labs(title = "Region 1: Wildfire Hazard Potential", x = "", y = "") +
   theme_bw() +

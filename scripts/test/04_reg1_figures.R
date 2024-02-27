72ea2a30a4347a3bee85509372d2fbcadc38313c
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-27 11:55:43 -0700

    maps of iterations 005 and 006

--- scripts/test/04_reg1_figures.R
+++ scripts/test/04_reg1_figures.R
@@ -13,8 +13,8 @@ library(ggdist)
 # Load the data
 fs_nf <- st_read("data/original/S_USA.AdministrativeForest.shp")
 fs_reg <- st_read("data/original/S_USA.AdministrativeRegion.shp")
-conus_attri <- rast("data/processed/iter_002_archetype_attribute_3km.tif")
-map.conus.k4 <- rast("data/processed/FCM_CONUS_k4_22feb2024.tif")
+conus_attri <- rast("data/processed/iter_006_archetype_attribute_3km.tif")
+map.conus <- rast("data/processed/fcm_conus_iter006_2024-02-27.tif")
 
 ## Reproject the forest service shapes to NAD83
 fs_nf.proj <- fs_nf %>% 
@@ -30,25 +30,25 @@ fs_reg.crop <- st_crop(fs_reg.proj, fs_nf.proj)
 #fs_subset.proj <- st_intersection(fs_subset.proj, fs_reg1.proj)
 
 ## Create a map of the clusters with the National Forest boundaries
-group.df <- map.conus.k4$Groups %>% as.data.frame(xy = TRUE)
+group.df <- map.conus$Groups %>% as.data.frame(xy = TRUE)
 
 fcm_nf_map <- ggplot() +
   geom_raster(aes(x = group.df$x, y = group.df$y, fill = as.factor(group.df$Groups))) +
   geom_sf(data = fs_nf.proj, fill = NA, color = "black", size = 2) +
   scale_fill_brewer(palette = "Set2") +
-  labs(title = "Fuzzy Cluster Map: k=4, m=1.5") +
+  labs(title = "Fuzzy Cluster Map: k=6, m=1.37") +
   theme(legend.position = "bottom")
 
-ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/fcm_nf_map_k4_", Sys.Date(), ".png"), plot = fcm_nf_map, width = 12, height = 12, dpi = 300)  
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/fcm_nf_map_iter006_", Sys.Date(), ".png"), plot = fcm_nf_map, width = 12, height = 12, dpi = 300)  
 
 fcm_reg_map <- ggplot() +
   geom_raster(aes(x = group.df$x, y = group.df$y, fill = as.factor(group.df$Groups))) +
   geom_sf(data = fs_reg.crop, fill = NA, color = "black", size = 2) +
   scale_fill_brewer(palette = "Set2") +
-  labs(title = "Fuzzy Cluster Map: k=4, m=1.5") +
+  labs(title = "Fuzzy Cluster Map: k=6, m=1.37") +
   theme(legend.position = "bottom")
 
-ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/fcm_reg_map_k4_", Sys.Date(), ".png"), plot = fcm_reg_map, width = 12, height = 12, dpi = 300)  
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/fcm_reg_map_iter006_", Sys.Date(), ".png"), plot = fcm_reg_map, width = 12, height = 12, dpi = 300)  
 
 
 ### And the Forest Region boundaries
@@ -58,6 +58,7 @@ lcv.df <- conus_attri$LCVScore %>% as.data.frame(xy = TRUE)
 rrl.df <- conus_attri$rrlrbn_ %>% as.data.frame(xy = TRUE)
 #whp.df <- reg1_attri$WHP_ID %>% as.data.frame(xy = TRUE)
 nlcd.df <- conus_attri$`NLCD Land Cover Class` %>% as.data.frame(xy = TRUE)
+nlcd.del.df <- conus_attri$Class_Names %>% as.data.frame(xy = TRUE)
 vdep.df <- conus_attri$LABEL %>% as.data.frame(xy = TRUE)
 fcv.df <- conus_attri$CLASSNAMES %>% as.data.frame(xy = TRUE)
 
@@ -71,7 +72,7 @@ rrl_map <- ggplot() +
         legend.title = element_text(size = 12),
         legend.position = "bottom")
 
-ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/rrl__conus_map_", Sys.Date(), ".png"), plot = rrl_map, width = 12, height = 12, dpi = 300)
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/rrl__conus_map_iter005", Sys.Date(), ".png"), plot = rrl_map, width = 12, height = 12, dpi = 300)
 
 lcv_map <- ggplot() +
   geom_raster(aes(x = lcv.df$x, y = lcv.df$y, fill = lcv.df$LCVScore)) +
@@ -83,7 +84,7 @@ lcv_map <- ggplot() +
         legend.title = element_text(size = 12),
         legend.position = "bottom")
 
-ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/lcv_conus_map_", Sys.Date(), ".png"), plot = lcv_map, width = 12, height = 12, dpi = 300)
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/lcv_conus_map_iter005", Sys.Date(), ".png"), plot = lcv_map, width = 12, height = 12, dpi = 300)
 
 nlcd_map <- ggplot() +
   geom_raster(aes(x = nlcd.df$x, y = nlcd.df$y, fill = nlcd.df$`NLCD Land Cover Class`)) +
@@ -95,7 +96,20 @@ nlcd_map <- ggplot() +
         legend.title = element_text(size = 12),
         legend.position = "bottom")
 
-ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/nlcd_conus_map_", Sys.Date(), ".png"), plot = nlcd_map, width = 12, height = 12, dpi = 300)
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/nlcd_conus_map_iter005", Sys.Date(), ".png"), plot = nlcd_map, width = 12, height = 12, dpi = 300)
+
+nlcd_del_map <- ggplot() +
+  geom_raster(aes(x = nlcd.del.df$x, y = nlcd.del.df$y, fill = nlcd.del.df$`NLCD Land Cover Class`)) +
+  geom_sf(data = fs_nf.proj, fill = NA, color = "black", size = 2) +
+  scale_fill_gradient("Land Cover Change", low = "darkgreen", high = "tan") +
+  labs(title = "National Land Cover Change", x = "", y = "") +
+  theme_bw() +
+  theme(plot.title = element_text(size=22), 
+        legend.title = element_text(size = 12),
+        legend.position = "bottom")
+
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/nlcd_del_conus_map_iter005", Sys.Date(), ".png"), plot = nlcd_del_map, width = 12, height = 12, dpi = 300)
+
 
 vdep_map <- ggplot() +
   geom_raster(aes(x = vdep.df$x, y = vdep.df$y, fill = vdep.df$LABEL)) +
@@ -107,7 +121,7 @@ vdep_map <- ggplot() +
         legend.title = element_text(size = 12),
         legend.position = "bottom")
 
-ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/vdep_conus_map_", Sys.Date(), ".png"), plot = vdep_map, width = 12, height = 12, dpi = 300)
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/vdep_conus_map_iter005", Sys.Date(), ".png"), plot = vdep_map, width = 12, height = 12, dpi = 300)
 
 fvc_map <- ggplot() +
   geom_raster(aes(x = fcv.df$x, y = fcv.df$y, fill = fcv.df$CLASSNAMES)) +
@@ -119,7 +133,7 @@ fvc_map <- ggplot() +
         legend.title = element_text(size = 12),
         legend.position = "bottom")
 
-ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/fvc_conus_map", Sys.Date(), ".png"), plot = fvc_map, width = 12, height = 12, dpi = 300)
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/fvc_conus_map_iter005_", Sys.Date(), ".png"), plot = fvc_map, width = 12, height = 12, dpi = 300)
 
 
 whp_map <- ggplot() +
@@ -142,7 +156,7 @@ ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/patch_test_", Sys.Date(), ".pn
 
 
 ## Create a stat_pointinterval plot
-names(conus_attri) <- c("vdep", "fvc", "nlcd", "lcv", "ruralurban_cc")
+names(conus_attri) <- c("vdep", "fvc", "nlcd", "nlcd_del", "lcv", "ruralurban_cc", "ealr_pfs")
 
 #
 all.vals <- c(map.conus[["Groups"]], conus_attri)
@@ -154,11 +168,13 @@ vals$lcv <- scale(vals$lcv)
 vals$ruralurban_cc <- scale(vals$ruralurban_cc)
 vals$vdep <- scale(vals$vdep)
 vals$nlcd <- scale(vals$nlcd)
+vals$nlcd_del <- scale(vals$nlcd_del)
 vals$fvc <- scale(vals$fvc)
-colnames(vals.k4) <- c("group", "vdep", "fvc", "nlcd", "lcv", "ruralurban_cc")
+vals$ealr_pfs <- scale(vals$ealr_pfs)
+colnames(vals) <- c("group", "vdep", "fvc", "nlcd", "nlcd_del", "lcv", "ruralurban_cc", "ealr_pfs")
 
 vals.df <- as.data.frame(vals) %>%
-  pivot_longer(., vdep:ruralurban_cc, names_to = "variable", values_to = "val")
+  pivot_longer(., vdep:ealr_pfs, names_to = "variable", values_to = "val")
 
 # not sure if this part is needed?
 vals.df.2 <- vals.df %>% 
@@ -175,7 +191,7 @@ multi.int.plot <- ggplot(data = vals.df.sum, aes(x = val, y = variable, color = 
   stat_pointinterval(position = "dodge") +
   scale_color_brewer(palette = "Set2")
 
-ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/conus_k4_multi_point_plot_", Sys.Date(), ".png"), plot = multi.int.plot, width = 12, height = 15, dpi = 300)  
+ggsave(paste0("~/Analysis/NEPA_Efficiency/figures/conus_multi_point_plot_iter006_", Sys.Date(), ".png"), plot = multi.int.plot, width = 12, height = 15, dpi = 300)  
 
 
                          

7bc9f3f062a784c8005cfa51557fc26a35726887
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-26 08:54:39 -0600

    script for creating FORISK mill capacity rasters

---
+++ scripts/final/00_forisk_analysis.R
@@ -0,0 +1,71 @@
+library(tidyverse)
+library(terra)
+library(sf)
+library(readr)
+library(ggplot2)
+library(ggmap)
+library(tigris)
+library(tmap)
+library(patchwork)
+library(units)
+
+#----Distance to mills and Mill Capacity "hotspots"
+# Pseudocode:
+# 1. Load the data (FORISK and county boundaries)
+# 2. Align the data
+# 3. Rasterize data
+# 4. Aggregate to 1.5km and 3km
+# 5. Calculate distance from mill points
+# 6. Mill capacity metric
+
+# 1. Load the data
+mill_sf <- read_sf(here::here("data/original/2024_Q1_Forisk_North_American_Ind_Cap_DB_Shape/Forisk_NA_FI_Capacity_DB_2024_Q1.shp"))
+
+
+##Get Continental US list
+us.abbr <- unique(fips_codes$state)[1:51]
+us.name <- unique(fips_codes$state_name)[1:51]
+
+us.states <- as.data.frame(cbind(us.abbr, us.name))
+colnames(us.states) <- c("state","STATENAME")
+us.states$state <- as.character(us.states$state)
+us.states$STATENAME <- as.character(us.states$STATENAME)
+continental.states <- us.states[us.states$state != "AK" & us.states$state != "HI" & us.states$state != "DC",] #only CONUS
+
+counties <- tigris::counties(state = continental.states$state, cb = TRUE)
+
+conus_mills <- mill_sf %>%
+  filter(Region != "Canada West") %>%
+  filter(Region != "Canada East") %>%
+  filter(State_Prov %in% continental.states$state)
+
+# 2. Align the data
+st_crs(conus_mills)
+st_crs(counties)
+
+identical(st_crs(conus_mills), st_crs(counties))
+
+#mill_county <- left_join()
+
+# 3. Rasterize the data
+
+ref_rast <- rast(here::here("data/processed/merged/WHP_merge3000m.tif"))
+
+mill_proj <- conus_mills %>% st_transform(., crs = crs(ref_rast))
+
+# Rasterize shapefiles with variables of interest
+tot_wood_rast <- rasterize(vect(mill_proj), ref_rast, field = "Total_Wood")
+status_rast <- rasterize(vect(mill_proj), ref_rast, field = "Status")
+
+# 3.5 Write rasters
+
+#writeRaster(tot_wood_rast, here::here("data/processed/tot_wood_cap.tif"), overwrite = TRUE)
+#writeRaster(status_rast, here::here("data/processed/mill_status.tif"), overwrite = TRUE)
+
+
+
+
+
+
+
+

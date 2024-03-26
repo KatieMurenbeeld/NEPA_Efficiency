c4140cc3b9f3c1ff6b4c3ee0f69cd89619800de7
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-26 16:29:42 -0600

    add mill distance and hotspot to raster stack

--- scripts/final/03_rst_to_fcm.R
+++ scripts/final/03_rst_to_fcm.R
@@ -12,6 +12,12 @@ library(viridis)
 #---Load the data-----
 ref_rast <- rast(here::here("data/processed/merged/WHP_merge3000m.tif"))
 arch_attri <- rast(here::here("data/processed/arch_attri_2024-03-22.tif"))
+mill_dist <- rast(here::here("data/processed/dist_to_mill.tif"))
+mill_cap_hot <- rast(here::here("data/processed/test_hotspot.tif"))
+
+# Check alignment 
+rast_stack <- c(arch_attri, mill_dist, mill_cap_hot)
+
 
 # ignore for now, file corrupted?
 #gpp_rast <- rast(here::here("data/original/CarbonFlux_2000_2013/Average_GPP_2000_2013.img"))
@@ -22,30 +28,30 @@ arch_attri <- rast(here::here("data/processed/arch_attri_2024-03-22.tif"))
 #---Process raster for use with geocmeans----
 
 # Update the names if you want 
-names(arch_attri)
+names(rast_stack)
 
-# Remove the wilderness and crithab for now
-arch_attri <- arch_attri[[c(1:11)]]
+# Select Variables for FCmeans
+rst_fcm <- rast_stack[[c("RUCC_20", "pct_pay", "lsscll_", "NAT_AME", "R_NET_M", "WHP", "distance_m")]]
 
 # Scale the data
-arch_attri_sc <- scale(arch_attri)
+rst_fcm_sc <- scale(rst_fcm)
 
 # Quickly investigate the correlation between the attributes
-layerCor(arch_attri_sc, "pearson", na.rm = TRUE)
+layerCor(rst_fcm, "pearson", na.rm = TRUE)
 
 # Convert to a simple list of SpatRaster
-dataset <- lapply(names(arch_attri_sc), function(n){
-  aband <- arch_attri_sc[[n]]
+dataset <- lapply(names(rst_fcm_sc), function(n){
+  aband <- rst_fcm_sc[[n]]
   return(aband)
 })
 
-names(dataset) <- names(arch_attri_sc)
+names(dataset) <- names(rst_fcm_sc)
 
 #---Find appropriate parameter values----
 ### Code below from https://jeremygelb.github.io/geocmeans/articles/web_vignettes/rasters.html
 # finding an appropriate k and m values
 FCMvalues <- select_parameters.mc(algo = "FCM", data = dataset, 
-                                  k = 2:11, m = seq(1.0,2.5,0.1), spconsist = FALSE, 
+                                  k = 2:6, m = seq(1.2,2.0,0.1), spconsist = FALSE, 
                                   indices = c("XieBeni.index", "Explained.inertia",
                                               "Negentropy.index", "Silhouette.index"),
                                   verbose = TRUE)
@@ -56,6 +62,7 @@ sil.idx <- ggplot(FCMvalues) +
   geom_text(aes(x = m, y = k, label = round(Silhouette.index,2)), size = 8) +
   coord_fixed(ratio=0.125) +
   scale_fill_viridis()
+sil.idx
 
 # plotting the explained inertia
 ex.inert <- ggplot(FCMvalues) + 
@@ -63,13 +70,14 @@ ex.inert <- ggplot(FCMvalues) +
   geom_text(aes(x = m, y = k, label = round(Explained.inertia,2)), size = 8) +
   scale_fill_viridis() +
   coord_fixed(ratio=0.125)
+ex.inert
 
 xieb.inert <- ggplot(FCMvalues) + 
   geom_raster(aes(x = m, y = k, fill = XieBeni.index)) + 
   geom_text(aes(x = m, y = k, label = round(XieBeni.index,2)), size = 8) +
   scale_fill_viridis() +
   coord_fixed(ratio=0.125)
-
+xieb.inert
 # Test out other clustering
 
 kmeans <- clusteringRaster(arch_attri_sc, k = 2:5, writeRasterData = FALSE, getSpatRaster = TRUE, 
@@ -90,15 +98,15 @@ plot(kr)
 
 
 # Test out running a FCM with k = 3 and m = 1.5
-FCM_result_k3 <- CMeans(dataset, k = 5, m = 1.1, standardize = FALSE)
-map.res.k3 <- rast(FCM_result_k3$rasters)
+FCM_result <- CMeans(dataset, k = 3, m = 1.2, standardize = FALSE)
+map.res <- rast(FCM_result$rasters)
 #writeRaster(map.res.k3[["Groups"]], filename = "data/processed/FCM_k3.tif")
 
 ## Visually review the clusters
 
-Maps.k3 <- mapClusters(object = FCM_result_k3, undecided = 0.45)
+Maps <- mapClusters(object = FCM_result, undecided = 0.45)
 
-test_fcm <- Maps.k3$ClusterPlot + theme(legend.position = "bottom") + 
+test_fcm <- Maps$ClusterPlot + theme(legend.position = "bottom") + 
   scale_fill_brewer(palette = "Set2")
 test_fcm
 

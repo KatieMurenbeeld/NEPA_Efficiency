7715799ab162e156f024d91663e70c27c5ccdad2
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-26 14:31:04 -0600

    have to make spatvector polygon using st_as_sf

--- scripts/final/00_forisk_analysis.R
+++ scripts/final/00_forisk_analysis.R
@@ -8,6 +8,7 @@ library(tigris)
 library(tmap)
 library(patchwork)
 library(units)
+library(spdep)
 
 #----Distance to mills and Mill Capacity "hotspots"
 # Pseudocode:
@@ -20,10 +21,6 @@ library(units)
 
 # 1. Load the data
 mill_sf <- read_sf(here::here("data/original/2024_Q1_Forisk_North_American_Ind_Cap_DB_Shape/Forisk_NA_FI_Capacity_DB_2024_Q1.shp"))
-
-
-
-
 
 ##Get Continental US list
 us.abbr <- unique(fips_codes$state)[1:51]
@@ -67,7 +64,7 @@ mill_proj$PRESENT <- 1
 mill_pres_rast <- rasterize(vect(mill_proj), ref_rast, field = "PRESENT", fun = "mean")
 tot_wood_rast <- rasterize(vect(mill_proj), ref_rast, field = "Total_Wood", fun = "sum")
 status_rast <- rasterize(vect(mill_proj), ref_rast, field = "Status")
-mill_ct_rast <- rasterize(vect(counties_proj), ref_rast, field = "pt_count", fun = "sum")
+mill_ct_rast <- rasterize(vect(counties_proj), ref_rast, field = "mill_count", fun = "sum")
 
 # 3.5 Write rasters
 
@@ -76,39 +73,27 @@ mill_ct_rast <- rasterize(vect(counties_proj), ref_rast, field = "pt_count", fun
 #writeRaster(mill_ct_rast, here::here("data/processed/mill_count.tif"), overwrite = TRUE)
 #writeRaster(mill_pres_rast, here::here("data/processed/mill_present.tif"), overwrite = TRUE)
 
-test_rast <- points_to_raster(mill_proj, nrow = 966, ncol = 1539, by = "Total_Wood", to.Raster = TRUE)
-
-r <- rasterize(vect(mill_proj), ref_rast, 'Total_Wood', fun = "sum")
-r2 <- rasterize(vect(mill_proj), ref_rast, 'Total_Wood', fun = "length('Total_Wood')")
-r3 <- rasterizeWin(vect(mill_proj), ref_rast, 'Total_Wood', win = "circle", fun = "max", pars = 30000)
-plot(r)
-plot(r3)
-
-fw <- focalMat(r, 3000, "circle") 
+fw <- focalMat(tot_wood_rast, 3000, "circle") 
 fw[fw > 0] <- 1 
 
-test_heat <- focal(r, fw, fun = mean, na.rm=T)
+test_heat <- focal(tot_wood_rast, fw, fun = mean, na.rm=T)
 plot(test_heat)
 plot(ref_rast)
 
 d2 <- distance(ref_rast, vect(mill_proj)) 
 plot(d2)
 
-
-
-
-
 # From https://michaelminn.net/tutorials/r-point-analysis/
 
 mill_proj$PRESENT <- 1
 getisraster <- rasterize(vect(mill_proj), ref_rast, field = 'PRESENT', fun = "sum")
-getisgrid <- as.polygons(getisraster)
-getisgrid2 <- raster::rasterToPolygons(getisraster)
-
+getisraster[is.na(getisraster[])] <- 0 
+getisgrid <- as.polygons(getisraster, round = FALSE, aggregate = FALSE)
+#getisgrid2 <- raster::rasterToPolygons(getisraster)
+getisgrid_casted <- st_as_sf(getisgrid, "POLYGON") 
 # Create the list of neighbors
 
-library(spdep)
-neighbors = poly2nb(getisgrid)
+neighbors = poly2nb(getisgrid_casted)
 weighted_neighbors = nb2listw(neighbors, zero.policy=T)
 
 geog.nearnb <- knn2nb(knearneigh(mill_proj, k = 1), sym=TRUE); #estimate distance to first neareset neighbor
@@ -117,9 +102,12 @@ weighted_neighbors <- nb2listw(nb.nearest, zero.policy=T)
 
 # Perform the local G analysis (Getis-Ord GI*)
 
-getisgrid$HOTSPOT <- as.vector(localG(getisgrid$sum, weighted_neighbors))
+getisgrid_casted$HOTSPOT <- as.vector(localG(mill_proj$Total_Wood, weighted_neighbors))
+breaks = c(-20, -1.96, -1, 1, 1.96, 20)
+palette=c("#0000FF80", "#8080FF80", "#FFFFFF80", "#FF808080", "#FF000080")
+col = palette[cut(getisgrid$HOTSPOT, breaks)]
 
-
+plot(getisgrid, col = col)
 
 plot(counties$geometry)
 

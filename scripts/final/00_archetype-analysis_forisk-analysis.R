b6eeb6fbf40a4691cebc37121ad7878b8c0e61eb
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-27 17:01:23 -0600

    renamed

--- scripts/final/00_archetype-analysis_forisk-analysis.R
+++ scripts/final/00_archetype-analysis_forisk-analysis.R
@@ -9,6 +9,7 @@ library(tmap)
 library(patchwork)
 library(units)
 library(spdep)
+library(spatstat)
 
 #----Distance to mills and Mill Capacity "hotspots"
 # Pseudocode:
@@ -58,6 +59,51 @@ counties$mill_count <- lengths(st_intersects(counties, conus_mills))
 ref_rast <- rast(here::here("data/processed/merged/WHP_merge3000m.tif"))
 mill_proj <- conus_mills %>% st_transform(., crs = crs(ref_rast))
 counties_proj <- counties %>% st_transform(., crs = crs(ref_rast))
+mill_counties_proj <- st_join(counties_proj, mill_proj) # make some county level metrics of capacity
+mill_counties_proj$Total_Wood <- replace_na(mill_counties_proj$Total_Wood, 0)
+  
+test_cap_sum <- mill_counties_proj %>% 
+  group_by(GEOID) %>%
+  summarise(sum = sum(Total_Wood))
+test_density <- density(mill_counties_proj$Total_Wood, bw=5)
+summary(test_density)
+plot(test_density, main = "Intensity")
+cap_pp <- as.ppp(st_geometry(mill_proj))
+plot(density(cap_pp, bw = 2))
+plot(counties_proj$geometry, add = TRUE)
+
+nb <- poly2nb(test_cap_sum, queen = TRUE) # queen shares point or border
+nbw <- nb2listw(nb, style = "W", zero.policy = TRUE)
+
+lmoran <- localmoran(test_cap_sum$sum, nbw, alternative = "two.sided")
+head(lmoran)
+
+test_cap_sum$lmp <- lmoran[, 5] # p-values are in column 5
+
+mp <- moran.plot(as.vector(scale(test_cap_sum$sum)), nbw)
+head(mp)
+
+test_cap_sum$quadrant <- NA
+# high-high
+test_cap_sum[((mp$x) >= 0 & (mp$wx) >= 0) & ((test_cap_sum$lmp) <= 0.05), "quadrant"]<- 1
+# low-low
+test_cap_sum[((mp$x) <= 0 & (mp$wx) <= 0) & (-is.na(test_cap_sum$lmp) <= 0.05), "quadrant"]<- 2
+# high-low
+test_cap_sum[((mp$x) >= 0 & (mp$wx) <= 0) & (-is.na(test_cap_sum$lmp) <= 0.05), "quadrant"]<- 3
+# low-high
+test_cap_sum[((mp$x) <= 0 & (mp$wx) >= 0) & (-is.na(test_cap_sum$lmp) <= 0.05), "quadrant"]<- 4
+# non-significant
+test_cap_sum[(is.na(test_cap_sum$lmp) > 0.05), "quadrant"] <- 5
+
+tm_shape(test_cap_sum) + tm_fill(col = "quadrant", title = "",
+                        breaks = c(1, 2, 3, 4, 5, 6),
+                        palette =  c("red", "blue", "lightpink", "skyblue2", "white"),
+                        labels = c("High-High", "Low-Low", "High-Low",
+                                   "Low-High", "Non-significant")) +
+  tm_legend(text.size = 1)  + tm_borders(alpha = 0.5) +
+  tm_layout(frame = FALSE,  title = "Clusters")  +
+  tm_layout(legend.outside = TRUE)
+
 
 mills <- ref_rast
 XMIN <- ext(mills)$xmin
@@ -107,13 +153,17 @@ mill_ct_rast <- rasterize(vect(counties_proj), ref_rast, field = "mill_count", f
 #writeRaster(mill_pres_rast, here::here("data/processed/mill_present.tif"), overwrite = TRUE)
 #writeRaster(mldist_crop, here::here("data/processed/mill_dist_open.tif"), overwrite = TRUE)
 
-fw <- focalMat(tot_wood_rast, 100000, "circle") 
+fw <- focalMat(tot_wood_rast, 50000, "circle") 
 fw[fw > 0] <- 1 
 
 test_heat <- focal(tot_wood_rast, fw, fun = mean, na.rm=T)
 plot(test_heat)
 plot(ref_rast)
 
+# could do kernel density estimate of mills themselves instead of distance to mills
+# distance to mills doesn't help distinguish between places near many mills or 1 mill
+# or anything about mill capacity
+# create sum of capacity in each county...
 
 mill_dist <- distance(ref_rast, vect(mill_proj)) 
 names(mill_dist) <- "distance_m"
@@ -130,14 +180,14 @@ getisgrid <- as.polygons(getisraster, round = FALSE, aggregate = FALSE)
 getisgrid_casted <- st_as_sf(getisgrid, "POLYGON") 
 # Create the list of neighbors
 
-neighbors = poly2nb(getisgrid_casted)
+neighbors = poly2nb(getisgrid_casted) #neighborhood is too small
 weighted_neighbors = nb2listw(neighbors, zero.policy=T)
 
 #geog.nearnb <- knn2nb(knearneigh(mill_proj, k = 1), sym=TRUE); #estimate distance to first neareset neighbor
 #nb.nearest <- dnearneigh(mill_proj, 0,  max( unlist(nbdists(geog.nearnb, mill_proj))))
 #weighted_neighbors <- nb2listw(nb.nearest, zero.policy=T)
 
-# Perform the local G analysis (Getis-Ord GI*)
+# Perform the local G analysis (Getis-Ord GI*?)
 
 getisgrid_casted$HOTSPOT <- as.vector(localG(getisgrid_casted$sum, weighted_neighbors))
 #breaks = c(-50, -1.96, -1, 1, 1.96, 50)

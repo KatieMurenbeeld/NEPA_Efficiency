07a3b87ec12d4bead3c2a377707e0c46517a7731
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-29 13:00:00 -0600

    new iterations

--- scripts/final/03_archetype-analysis_rst-to-fcm.R
+++ scripts/final/03_archetype-analysis_rst-to-fcm.R
@@ -11,20 +11,21 @@ library(viridis)
 
 #---Load the data-----
 ref_rast <- rast(here::here("data/processed/merged/WHP_merge3000m.tif"))
-arch_attri <- rast(here::here("data/processed/arch_attri_03_2024-03-27.tif"))
+arch_attri <- rast(here::here("data/processed/arch_attri_04_2024-03-29.tif"))
 mill_dist <- rast(here::here("data/processed/mill_dist_open.tif"))
 mill_cap_hot <- rast(here::here("data/processed/test_hotspot.tif"))
 agc_flux <- rast("~/Analysis/NEPA_Efficiency/data/original/forest_aboveground_carbon_flux_ED_GEDI_ICESat2.tif")
 agc_stock <- rast("~/Analysis/NEPA_Efficiency/data/original/forest_aboveground_carbon_stock_ED_GEDI.tif")
+
 # ignore for now, file corrupted?
 #gpp_rast <- rast(here::here("data/original/CarbonFlux_2000_2013/Average_GPP_2000_2013.img"))
 
 #---Process the original rasters----
 # Resample then crop
-agc_stock_proj <- project(agc_stock, 
-                          y = "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs")
+#agc_stock_proj <- project(agc_stock, 
+#                          y = "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs")
 
-agc_stock_crop <- crop(agc_stock_proj, ref_rast, mask = TRUE)
+#agc_stock_crop <- crop(agc_stock_proj, ref_rast, mask = TRUE)
 
 
 
@@ -37,8 +38,11 @@ rast_stack <- c(arch_attri, mill_dist)
 names(rast_stack)
 
 # Select Variables for FCmeans
-rst_fcm <- rast_stack[[c("RUCC_20", "av_vt_n", "pct_pay", "R_NET_M", "WHP", "last")]]
-#writeRaster(rst_fcm, paste0("data/processed/rast_fcm_02_", Sys.Date(), ".tif"), overwrite = TRUE)
+rst_fcm <- rast_stack[[c("distance_to_crithab_m", "distance_to_wilderness_m", 
+                         "last", "WHP", "recreation", "LIF_PFS", "LMI_PFS", 
+                         "EALR_PFS", "av_vt_n", "pct_pay")]]
+names(rst_fcm)
+#writeRaster(rst_fcm, paste0("data/processed/rast_fcm_04_", Sys.Date(), ".tif"), overwrite = TRUE)
 
 # Scale the data
 rst_fcm_sc <- scale(rst_fcm)
@@ -58,9 +62,9 @@ names(dataset) <- names(rst_fcm_sc)
 ### Code below from https://jeremygelb.github.io/geocmeans/articles/web_vignettes/rasters.html
 # finding an appropriate k and m values
 FCMvalues <- select_parameters.mc(algo = "FCM", data = dataset, 
-                                  k = 2:6, m = seq(1.2,2.0,0.1), spconsist = FALSE, 
+                                  k = 2:6, m = seq(1.0,2.5,0.25), spconsist = FALSE, 
                                   indices = c("XieBeni.index", "Explained.inertia",
-                                              "Negentropy.index", "Silhouette.index"),
+                                              "Silhouette.index"),
                                   verbose = TRUE)
 
 # plotting the silhouette index values
@@ -70,6 +74,8 @@ sil.idx <- ggplot(FCMvalues) +
   coord_fixed(ratio=0.125) +
   scale_fill_viridis()
 sil.idx
+ggsave(here::here("figures/FCM_05_sil_idx.png"), sil.idx, 
+       width = 12, height = 12, dpi = 300)
 
 # plotting the explained inertia
 ex.inert <- ggplot(FCMvalues) + 
@@ -78,53 +84,29 @@ ex.inert <- ggplot(FCMvalues) +
   scale_fill_viridis() +
   coord_fixed(ratio=0.125)
 ex.inert
+ggsave(here::here("figures/FCM_05_ex_inert.png"), ex.inert, 
+       width = 12, height = 12, dpi = 300)
 
-xieb.inert <- ggplot(FCMvalues) + 
-  geom_raster(aes(x = m, y = k, fill = XieBeni.index)) + 
-  geom_text(aes(x = m, y = k, label = round(XieBeni.index,2)), size = 8) +
-  scale_fill_viridis() +
-  coord_fixed(ratio=0.125)
-xieb.inert
-# Test out other clustering
-
-kmeans <- clusteringRaster(arch_attri_sc, k = 2:5, writeRasterData = FALSE, getSpatRaster = TRUE, 
-                 method="kmeans", calcIntCriteria = FALSE, crit = c("Silhouette"), 
-                 intCritSampSize = 20000, verbose = TRUE)
-
-# use kmeans()?
-v <- na.omit(values(arch_attri_sc))
-set.seed(99)
-kmn <- kmeans(v, centers=4, iter.max = 500, nstart = 5, algorithm="Lloyd")
-
-kr <- rast(arch_attri_sc, nlyr=1)
-i <- attr(v, "na.action")
-j <- (1:ncell(arch_attri_sc))[-i]
-kr[j] <- kmn$cluster
-
-plot(kr)
-
-
-# Test out running a FCM 
-FCM_result <- CMeans(dataset, k = 5, m = 1.2, standardize = FALSE)
+#---Run the FCM---- 
+FCM_result <- CMeans(dataset, k = 5, m = 2.25, standardize = FALSE)
 map.res <- rast(FCM_result$rasters)
-#writeRaster(map.res[["Groups"]], filename = paste0("data/processed/FCM_02", Sys.Date(), ".tif"))
-
-## Visually review the clusters
-
-Maps <- mapClusters(object = FCM_result, undecided = 0.45)
-
-test_fcm <- Maps$ClusterPlot + theme(legend.position = "bottom") + 
-  scale_fill_brewer(palette = "Set2")
-test_fcm
-
-#ggsave("test_fcm.png", plot = test_fcm, width = 12, height = 12, dpi = 300)
+writeRaster(map.res[["Groups"]], filename = paste0("data/processed/FCM_05_", Sys.Date(), ".tif"))
 
 
+##---save the iteration, k, m as a dataframe----
+#aa_iteration <- data.frame(iteration_name = character(),
+#                           attris = character(),
+#                           k = numeric(),
+#                           m = numeric())
 
+iteration_name <- "FCM_05"
+attris <- paste(names(rst_fcm), collapse= ", ")
+k <- 5
+m <- 2.25
 
+aa_iteration[nrow(aa_iteration) + 1,] <- list(iteration_name, attris, k, m)
 
-
-
+write_csv(aa_iteration, here::here("data/processed/aa_iteration.csv"), append = TRUE)
 
 
 

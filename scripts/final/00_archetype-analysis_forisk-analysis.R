0393e75812c375f568988b771dae5a42818ab7c6
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-05-20 10:05:25 -0600

    Calculate and interpolate the change in mill capacity.
    Calculate and interpolate the percent change in mill capacity using nearest neighbors and 3 neighbor sizes.
    
    Also make these categorical and rasterize
    
    Deleted old code where I tried to create a mill capacity hotspot. See previous version for the Morans-I plot of county level mill capacity.

--- scripts/final/00_archetype-analysis_forisk-analysis.R
+++ scripts/final/00_archetype-analysis_forisk-analysis.R
@@ -38,8 +38,8 @@ counties <- tigris::counties(state = continental.states$state, cb = TRUE)
 conus_mills <- mill_sf %>%
   filter(Region != "Canada West") %>%
   filter(Region != "Canada East") %>%
-  filter(State_Prov %in% continental.states$state) %>%
-  filter(Status == "Open")
+  filter(State_Prov %in% continental.states$state) #%>% # need to keep all mills in order to calculate change in mill capacity for mills that have closed
+  #filter(Status == "Open")
 
 # What is the distribution of total wood capacity?
 
@@ -51,156 +51,80 @@ st_crs(counties)
 
 identical(st_crs(conus_mills), st_crs(counties))
 
-#mill_county <- left_join()
+# Create a mill count variable for each county
 counties$mill_count <- lengths(st_intersects(counties, conus_mills))
 
-# 3. Rasterize the data
+# Calculate the change in mill capacity from 2019-2023 (5 years)
+## For mills that came "online" set the CPT_2019 to 1.0
 
-ref_rast <- rast(here::here("data/processed/merged/WHP_merge3000m.tif"))
+conus_mills$CPT_2019[conus_mills$CPT_2019 == 0.0] <- 1.0 
+
+conus_mills <- conus_mills %>%
+  mutate(millcap_5yr = (CPT_2023 - CPT_2019) / CPT_2019)
+
+### Make categorical. Could also make this only 3 categories (decrease, no change, increase in order to protect identities)
+conus_mills <- conus_mills %>%
+  mutate(change_millcap = case_when(millcap_5yr == -1.0 ~ "mill closed", 
+                                    millcap_5yr > -1.0 & millcap_5yr < 0.0 ~ "decrease",
+                                    millcap_5yr == 0.0 ~ "no change",
+                                    millcap_5yr > 0.0 & CPT_2019 != 1.0 ~ "increase",
+                                    millcap_5yr > 0.0 & CPT_2019 == 1.0 ~"new mill"))
+
+# Check for missing data
+print ("Row and Col positions of NA values") 
+which(is.na(conus_mills$millcap_5yr), arr.ind = TRUE)
+
+## Interpolate del mill capacity
+nodes <- st_make_grid(counties,
+                      n = c(50,50),
+                      what = "centers")
+dist <- distance(vect(nodes), vect(conus_mills))
+nearest_conus <- apply(dist, 1, function(x) which(x == min(x)))
+nearest_conus1 <- as.data.frame(nearest_conus[1:2500])
+nearest_conus2 <- nearest_conus1 %>% filter(row_number() == 2)
+#nearest_conus3 <- as.array(nearest_conus2)
+nearest <- as.numeric(as.vector(nearest_conus2[1,]))
+millcap5.nn <- conus_mills$millcap_5yr[nearest]
+currcap.nn <- conus_mills$Current_Ca[nearest]
+totwood.nn <- conus_mills$Total_Wood[nearest]
+preds <- st_as_sf(nodes)
+preds$millcap5 <- millcap5.nn
+preds$currcap <- currcap.nn
+preds$totwood <- totwood.nn
+preds <- as(preds, "Spatial")
+sp::gridded(preds) <- TRUE
+preds.rast <- rast(preds)
+
+mc5sf05 <- gstat(id = "millcap_5yr", formula = millcap_5yr~1, data=conus_mills,  nmax=7, set=list(idp = 0.5))
+mc5sf1 <- gstat(id = "millcap_5yr", formula = millcap_5yr~1, data=conus_mills,  nmax=7, set=list(idp = 1))
+mc5sf2 <- gstat(id = "millcap_5yr", formula = millcap_5yr~1, data=conus_mills,  nmax=7, set=list(idp = 2))
+
+interpolate_gstat <- function(model, x, crs, ...) {
+  v <- st_as_sf(x, coords=c("x", "y"), crs=crs)
+  p <- predict(model, v, ...)
+  as.data.frame(p)[,1:2]
+}
+
+zmc5sf05 <- interpolate(preds.rast, mc5sf05, debug.level=0, fun=interpolate_gstat, crs=crs(preds.rast), index=1)
+zmc5sf1 <- interpolate(preds.rast, mc5sf1, debug.level=0, fun=interpolate_gstat, crs=crs(preds.rast), index=1)
+zmc5sf2 <- interpolate(preds.rast, mc5sf2, debug.level=0, fun=interpolate_gstat, crs=crs(preds.rast), index=1)
+
+# Crop the mill capacity change predictions to the counties data. These are rasters.
+zmc5sf05_crop <- crop(zmc5sf05, counties, mask = TRUE) 
+zmc5sf1_crop <- crop(zmc5sf1, counties, mask = TRUE)
+zmc5sf2_crop <- crop(zmc5sf2, counties, mask = TRUE)
+
+# 3. Rasterize the data using the raster created in the 00_archetype-analysis_download-prep-fire.R
+
+ref_rast <- rast(here::here("data/processed/merged/WHP_merge3000m.tif")) 
 mill_proj <- conus_mills %>% st_transform(., crs = crs(ref_rast))
-counties_proj <- counties %>% st_transform(., crs = crs(ref_rast))
-mill_counties_proj <- st_join(counties_proj, mill_proj) # make some county level metrics of capacity
-mill_counties_proj$Total_Wood <- replace_na(mill_counties_proj$Total_Wood, 0)
-  
-test_cap_sum <- mill_counties_proj %>% 
-  group_by(GEOID) %>%
-  summarise(sum = sum(Total_Wood))
-test_density <- density(mill_counties_proj$Total_Wood, bw=5)
-summary(test_density)
-plot(test_density, main = "Intensity")
-cap_pp <- as.ppp(st_geometry(mill_proj))
-plot(density(cap_pp, bw = 2))
-plot(counties_proj$geometry, add = TRUE)
-
-nb <- poly2nb(test_cap_sum, queen = TRUE) # queen shares point or border
-nbw <- nb2listw(nb, style = "W", zero.policy = TRUE)
-
-lmoran <- localmoran(test_cap_sum$sum, nbw, alternative = "two.sided")
-head(lmoran)
-
-test_cap_sum$lmp <- lmoran[, 5] # p-values are in column 5
-
-mp <- moran.plot(as.vector(scale(test_cap_sum$sum)), nbw)
-head(mp)
-
-test_cap_sum$quadrant <- NA
-# high-high
-test_cap_sum[((mp$x) >= 0 & (mp$wx) >= 0) & ((test_cap_sum$lmp) <= 0.05), "quadrant"]<- 1
-# low-low
-test_cap_sum[((mp$x) <= 0 & (mp$wx) <= 0) & (-is.na(test_cap_sum$lmp) <= 0.05), "quadrant"]<- 2
-# high-low
-test_cap_sum[((mp$x) >= 0 & (mp$wx) <= 0) & (-is.na(test_cap_sum$lmp) <= 0.05), "quadrant"]<- 3
-# low-high
-test_cap_sum[((mp$x) <= 0 & (mp$wx) >= 0) & (-is.na(test_cap_sum$lmp) <= 0.05), "quadrant"]<- 4
-# non-significant
-test_cap_sum[(is.na(test_cap_sum$lmp) > 0.05), "quadrant"] <- 5
-
-tm_shape(test_cap_sum) + tm_fill(col = "quadrant", title = "",
-                        breaks = c(1, 2, 3, 4, 5, 6),
-                        palette =  c("red", "blue", "lightpink", "skyblue2", "white"),
-                        labels = c("High-High", "Low-Low", "High-Low",
-                                   "Low-High", "Non-significant")) +
-  tm_legend(text.size = 1)  + tm_borders(alpha = 0.5) +
-  tm_layout(frame = FALSE,  title = "Clusters")  +
-  tm_layout(legend.outside = TRUE)
-
-
-mills <- ref_rast
-XMIN <- ext(mills)$xmin
-XMAX <- ext(mills)$xmax
-YMIN <- ext(mills)$ymin
-YMAX <- ext(mills)$ymax
-aspectRatio <- (YMAX-YMIN)/(XMAX-XMIN)
-cellSize <- 3000
-NCOLS <- as.integer((XMAX-XMIN)/cellSize)
-NROWS <- as.integer(NCOLS * aspectRatio)
-templateRas <- rast(ncol=NCOLS, nrow=NROWS, 
-                    xmin=XMIN, xmax=XMAX, ymin=YMIN, ymax=YMAX,
-                    vals=1, crs=crs(mills))
-
-mlls <- rasterize(vect(mill_proj),templateRas)
-#ext(mlls) <- ext(ref_rast)
-mldist <- terra::distance(mlls)
-plot(mldist)
-points(mlls)
-
-mldist_crop <- crop(mldist, ref_rast, mask = TRUE)
-plot(mldist_crop)
-
-# Classify the distances
-distclasses <-matrix(c(1,100000,1, 
-                       100000,250000,2, 
-                       250000,500000,3,
-                       500000, 1000000,4), 
-                     ncol=3, 
-                     byrow=TRUE)
-distclass <- terra::classify(mldist_crop, rcl = distclasses, include.lowest = TRUE)
-plot(distclass)
-
-# Rasterize shapefiles with variables of interest
-
-mill_proj$PRESENT <- 1
-mill_pres_rast <- rasterize(vect(mill_proj), ref_rast, field = "PRESENT", fun = "mean")
-tot_wood_rast <- rasterize(vect(mill_proj), ref_rast, field = "Total_Wood", fun = "sum")
-status_rast <- rasterize(vect(mill_proj), ref_rast, field = "Status")
-mill_ct_rast <- rasterize(vect(counties_proj), ref_rast, field = "mill_count", fun = "sum")
+millchange_rast <- rasterize(vect(mill_proj), ref_rast, field = "change_millcap")
 
 # 3.5 Write rasters
 
-#writeRaster(tot_wood_rast, here::here("data/processed/tot_wood_cap.tif"), overwrite = TRUE)
-#writeRaster(status_rast, here::here("data/processed/mill_status.tif"), overwrite = TRUE)
-#writeRaster(mill_ct_rast, here::here("data/processed/mill_count.tif"), overwrite = TRUE)
-#writeRaster(mill_pres_rast, here::here("data/processed/mill_present.tif"), overwrite = TRUE)
-#writeRaster(mldist_crop, here::here("data/processed/mill_dist_open.tif"), overwrite = TRUE)
+writeRaster(millchange_rast, here::here("data/processed/millchange_cap.tif"), overwrite = TRUE)
+writeRaster(zmc5sf05_crop, here::here("data/processed/millchangecap_interp-05.tif"), overwrite = TRUE)
+writeRaster(zmc5sf1_crop, here::here("data/processed/millchangecap_interp-1.tif"), overwrite = TRUE)
+writeRaster(zmc5sf2_crop, here::here("data/processed/millchangecap_interp-2.tif"), overwrite = TRUE)
 
-fw <- focalMat(tot_wood_rast, 50000, "circle") 
-fw[fw > 0] <- 1 
 
-test_heat <- focal(tot_wood_rast, fw, fun = mean, na.rm=T)
-plot(test_heat)
-plot(ref_rast)
-
-# could do kernel density estimate of mills themselves instead of distance to mills
-# distance to mills doesn't help distinguish between places near many mills or 1 mill
-# or anything about mill capacity
-# create sum of capacity in each county...
-
-mill_dist <- distance(ref_rast, vect(mill_proj)) 
-names(mill_dist) <- "distance_m"
-#writeRaster(mill_dist), here::here("data/processed/dist_to_mill.tif"), overwrite = TRUE)
-plot(mill_dist)
-
-# From https://michaelminn.net/tutorials/r-point-analysis/
-
-mill_proj$PRESENT <- 1
-getisraster <- rasterize(vect(mill_proj), ref_rast, field = 'PRESENT', fun = "sum")
-getisraster[is.na(getisraster[])] <- 0 
-getisgrid <- as.polygons(getisraster, round = FALSE, aggregate = FALSE)
-#getisgrid2 <- raster::rasterToPolygons(getisraster)
-getisgrid_casted <- st_as_sf(getisgrid, "POLYGON") 
-# Create the list of neighbors
-
-neighbors = poly2nb(getisgrid_casted) #neighborhood is too small
-weighted_neighbors = nb2listw(neighbors, zero.policy=T)
-
-#geog.nearnb <- knn2nb(knearneigh(mill_proj, k = 1), sym=TRUE); #estimate distance to first neareset neighbor
-#nb.nearest <- dnearneigh(mill_proj, 0,  max( unlist(nbdists(geog.nearnb, mill_proj))))
-#weighted_neighbors <- nb2listw(nb.nearest, zero.policy=T)
-
-# Perform the local G analysis (Getis-Ord GI*?)
-
-getisgrid_casted$HOTSPOT <- as.vector(localG(getisgrid_casted$sum, weighted_neighbors))
-#breaks = c(-50, -1.96, -1, 1, 1.96, 50)
-#palette=c("#0000FF80", "#8080FF80", "#FFFFFF80", "#FF808080", "#FF000080")
-#col = palette[cut(getisgrid_casted$HOTSPOT, breaks)]
-
-#plot(getisgrid_casted, col = col)
-
-hotspot <- rasterize(vect(getisgrid_casted), ref_rast, field = "HOTSPOT")
-writeRaster(hotspot, here::here("data/processed/test_hotspot.tif"), overwrite = TRUE)
-
-plot(hotspot, xlim=c(-2300000, -1400000), ylim=c(2500000, 3150000))
-plot(counties_proj$geometry, border = "grey", add = TRUE)
-
-plot(d2, xlim=c(-2500000, -1000000), ylim=c(1800000, 3150000))
-plot(counties_proj$geometry, border = "grey", add = TRUE)

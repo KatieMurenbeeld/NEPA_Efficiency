8f6830e7dae2c9ecdb5feef9b745234ec6530395
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-05-21 15:56:20 -0600

    use sf_use_s2(FALSE) to turn off using spherical geometry was able to calculate proportion of county area covered by federal governance, but I think I need to basically merge all of the Fed polygons into one giant multipolygon? Or I can sum all of the values by county?

--- scripts/final/00_archetype-analysis_PADUS-data-prep.R
+++ scripts/final/00_archetype-analysis_PADUS-data-prep.R
@@ -56,15 +56,23 @@ identical(st_crs(conus_fedp), st_crs(counties))
 ## Make the multisurface into multipolygons
 conus_fedp <- conus_fedp %>% st_cast("MULTIPOLYGON")
 
+# Turn off using spherical geometry??
+sf_use_s2(FALSE)
 ## Check and fix validity
-st_is_valid(conus_fedp)
+all(st_is_valid(conus_fedp))
 conus_fedp_val <- st_make_valid(conus_fedp)
-st_is_valid(conus_fedp_val)
+all(st_is_valid(conus_fedp_val))
+
+
+#conus_fedp_val_buff <- st_buffer(conus_fedp_val, dist = 0)
+
 
 ## Check for and remove empty geometries
-st_is_empty(conus_fedp_val)
-conus_fedp_val_noempty = conus_fedp_val[!st_is_empty(conus_fedp_val),]
-st_is_empty(conus_fedp_val_noempty)
+all(st_is_empty(conus_fedp_val))
+#conus_fedp_val_noempty = conus_fedp_val[!st_is_empty(conus_fedp_val),]
+#st_is_empty(conus_fedp_val_noempty)
+
+
 
 # 4. Rasterize the data
 ## Load the reference raster from fire-data-prep
@@ -77,4 +85,26 @@ conus_fed_val_noempty_p <- st_transform(conus_fedp_val_noempty, crs(ref_rast))
 fed_rast <- rasterize(vect(conus_fed_val_noempty_p), ref_rast, field = "Mang_Name")
 #writeRaster(fed_rast, here::here("data/processed/fed_lands_3000m.tif"), overwrite = TRUE)
 
-# 5. Calculate...
+# 5. Calculate the area of overlapping Fed and county polygons (maybe I didn't need to figure out the rasterization yet...)
+## from stack overflow https://gis.stackexchange.com/questions/362466/calculate-percentage-overlap-of-2-sets-of-polygons-in-r
+
+## Calculate area and tidy up
+intersect_pct <- st_intersection(counties, conus_fedp_val) %>% 
+  mutate(intersect_area = st_area(.)) %>%   # create new column with shape area
+  dplyr::select(GEOID, intersect_area) %>%   # only select columns needed to merge
+  st_drop_geometry()  # drop geometry as we don't need it
+
+# Create a fresh area variable for counties
+counties <- mutate(counties, county_area = st_area(counties))
+
+# Merge by county name
+counties <- merge(counties, intersect_pct, by = "GEOID", all.x = TRUE)
+
+# Calculate coverage
+counties <- counties %>% 
+  mutate(coverage = as.numeric(intersect_area/county_area))
+
+
+
+
+

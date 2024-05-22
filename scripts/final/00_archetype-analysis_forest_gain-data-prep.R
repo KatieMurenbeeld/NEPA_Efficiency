73262e06f8899038c9e8240905861b8ee3bfb0a4
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-05-22 11:28:53 -0600

    Started new script for downloading and processing the forest gain data from Hansen et al 2013 Listed the 10 X 10 degree grids for contiguous US

---
+++ scripts/final/00_archetype-analysis_forest_gain-data-prep.R
@@ -0,0 +1,12 @@
+
+
+#---Note---------
+# This data is downloaded from 
+# https://storage.googleapis.com/earthenginepartners-hansen/GFC-2023-v1.11/download.html
+# one has to download the desire 10 x 10 degree boxes
+# For the contiguous US this includes 
+# 50N, 130W; 50N, 120W; 50N, 110W; 50N, 100W; 50N, 90W; 50N, 80W; 50N, 70W
+# 40N, 130W; 40N, 120W; 40N, 110W; 40N, 100W; 40N, 90W; 40N, 80W
+# 30N, 120W; 30N, 110W; 30N, 100W; 30N, 90W; 30N, 80W
+
+# Download the data

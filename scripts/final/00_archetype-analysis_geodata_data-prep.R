9706f4c73e41a141412bd9542214f50502b97249
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-05-28 14:47:52 -0600

    added comments about geodata package being archived by CRAN manually downloaded worldclim precip and temp and added rast()

--- scripts/final/00_archetype-analysis_geodata_data-prep.R
+++ scripts/final/00_archetype-analysis_geodata_data-prep.R
@@ -5,7 +5,7 @@
 library(sf)
 library(raster)
 library(sp)
-library(geodata)
+library(geodata) # this package removed from CRAN May 27, 2024...
 library(terra)
 library(tigris)
 library(dplyr)
@@ -21,15 +21,18 @@ proj <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps
 
 # 1. Download the data using the geodata package
 
-r_prec <- geodata::worldclim_country(country = "USA", var = "prec", res = 0.5, path = here::here("data/original/"))
-r_temp <- geodata::worldclim_country(country = "USA", var = "tavg", res = 0.5, path = here::here("data/original/"))
-r_bio <- geodata::worldclim_country(country = "USA", var = "bio", res = 0.5, path = here::here("data/original/"))
+#r_prec <- geodata::worldclim_country(country = "USA", var = "prec", res = 0.5, path = here::here("data/original/"))
+r_prec <- rast(here::here("data/original/wc2.1_country/USA_wc2.1_30s_prec.tif"))
+#r_temp <- geodata::worldclim_country(country = "USA", var = "tavg", res = 0.5, path = here::here("data/original/"))
+r_temp <- rast(here::here("data/original/wc2.1_country/USA_wc2.1_30s_tavg.tif"))
+#r_bio <- geodata::worldclim_country(country = "USA", var = "bio", res = 0.5, path = here::here("data/original/"))
+#r_bio <- rast(here::here("data/original/wc2.1_country/USA_wc2.1_30s_bio.tif"))
 r_ele <- geodata::elevation_30s(country = "US", path = here::here("data/original/"))
 r_tt <- geodata::travel_time(to = "city", size = 7, up = TRUE, path = here::here("data/original/"))
 
 ## Get the mean annual temperature, temperature seasonality, precipitation, and precipitation seasonality 
-temp_prec <- r[[c(1, 4, 12, 15)]]
-names(temp_prec) <- c("Temp", "Seas_T", "Prec", "Seas_P")
+#temp_prec <- r_bio[[c(1, 4, 12, 15)]]
+#names(temp_prec) <- c("Temp", "Seas_T", "Prec", "Seas_P")
 
 # 2. Crop to CONUS
 ## Using tigris, download the state boundaries
@@ -40,11 +43,17 @@ states <- states %>%
   filter(STUSPS != "AK" & STUSPS != "HI" & STUSPS != "DC") %>%
   filter(GEOID < 60)
 
-## Set the crs of r_tt to states
+## Set the crs to the projection
+crs(r_prec) <- proj
+crs(r_temp) <- proj
 crs(r_ele) <- proj
 crs(r_tt) <- proj
+
 states <- st_transform(states, proj)
 
+r_prec_conus <- crop(r_prec, states, mask = TRUE)
+r_temp_conus <- crop(r_temp, states, mask = TRUE)
+r_ele_conus <- crop(r_ele, states, mask = TRUE)
 r_tt_conus <- crop(r_tt, states, mask = TRUE)
 
 # 3. Resample to 1.5km and 3km resolution

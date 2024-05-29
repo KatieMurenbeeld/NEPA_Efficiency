756cc5dc7ad7e39eafa10aec4e776f3654cba0a3
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-05-28 21:43:23 -0600

    added comments for packages to load loaded tmin and tmax so I could use biovars function from dismo package to get the temp and precip seasonality

--- scripts/final/00_archetype-analysis_geodata_data-prep.R
+++ scripts/final/00_archetype-analysis_geodata_data-prep.R
@@ -2,13 +2,14 @@
 ## Download elevation data and calculate a topographic complexity index
 
 # 0. Load libraries and set projection
-library(sf)
-library(raster)
-library(sp)
+library(sf) # for working with vector data
+library(raster) # for working with rasters
+library(sp) # for working with spatial (vector) data
 library(geodata) # this package removed from CRAN May 27, 2024...
-library(terra)
-library(tigris)
-library(dplyr)
+library(terra) # for working with rasters
+library(tigris) # needed for state/CONUS boundaries
+library(dplyr) # for manipulating dataframes 
+library(dismo) # needed to calculate biovars
 
 #proj <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83"
 
@@ -22,6 +23,8 @@ library(dplyr)
 # 1. Download the data using the geodata package
 
 #r_prec <- geodata::worldclim_country(country = "USA", var = "prec", res = 0.5, path = here::here("data/original/"))
+R_tmin <- geodata::worldclim_country(country = "USA", var = "tmin", res = 0.5, path = here::here("data/original"))
+r_tmax <- geodata::worldclim_country(country = "USA", var = "tmax", res = 0.5, path = here::here("data/original"))
 r_prec <- rast(here::here("data/original/wc2.1_country/USA_wc2.1_30s_prec.tif"))
 #r_temp <- geodata::worldclim_country(country = "USA", var = "tavg", res = 0.5, path = here::here("data/original/"))
 r_temp <- rast(here::here("data/original/wc2.1_country/USA_wc2.1_30s_tavg.tif"))
@@ -55,7 +58,8 @@ r_prec_conus <- crop_project(r_prec, states)
 r_temp_conus <- crop_project(r_temp, states)
 r_ele_conus <- crop_project(r_ele, states)
 r_tt_conus <- crop_project(r_tt, states)
-
+r_tmin_conus <- crop_project(R_tmin, states)
+r_tmax_conus <- crop_project(r_tmax, states)
 
 # 3. Resample to 1.5km and 3km resolution
 ## Using the WHP raster as a reference
@@ -71,18 +75,27 @@ r_tt_1500 <- resamp(r_tt_conus, ref_rast1.5, "bilinear")
 r_tt_3000 <- resamp(r_tt_conus, ref_rast3, "bilinear")
 r_prec_1500 <- resamp(r_prec_conus, ref_rast1.5, "bilinear")
 r_prec_3000 <- resamp(r_prec_conus, ref_rast3, "bilinear")
+r_tmin_1500 <- resamp(r_tmin_conus, ref_rast1.5, "bilinear")
+r_tmin_3000 <- resamp(r_tmin_conus, ref_rast3, "bilinear")
+r_tmax_1500 <- resamp(r_tmax_conus, ref_rast1.5, "bilinear")
+r_tmax_3000 <- resamp(r_tmax_conus, ref_rast3, "bilinear")
 r_temp_1500 <- resamp(r_temp_conus, ref_rast1.5, "bilinear")
 r_temp_3000 <- resamp(r_temp_conus, ref_rast3, "bilinear")
 
+bio_conus_1500 <- biovars(brick(r_prec_1500), brick(r_tmin_1500), brick(r_tmax_1500))
+bio_conus_3000 <- biovars(brick(r_prec_3000), brick(r_tmin_3000), brick(r_tmax_3000))
 
+r_prec_seas_1500 <- bio_conus_1500$bio15
 
-proj_test <- project(r_tt_conus, crs(proj))
-proj_test2 <- project(r_tt_conus, crs(ref_rast1.5))
-proj_test3 <- project(r_tt_conus, crs(ref_rast3))
+r_temp_seas_1500 <- bio_conus_1500$bio04
 
-resamp_test <- resample(proj_test2, ref_rast1.5, method = "bilinear")
-resamp_test2 <- resample(proj_test3, ref_rast3, method = "bilinear")
+# Calculate the difference in temperature between summer and winter
+del_temp_1500 <- r_temp_1500$USA_wc2.1_30s_tavg_7 - r_temp_1500$USA_wc2.1_30s_tavg_1
+del_temp_3000 <- r_temp_3000$USA_wc2.1_30s_tavg_7 - r_temp_3000$USA_wc2.1_30s_tavg_1
 
-identical(crs(proj_test), crs(ref_rast1.5))
-identical(crs(proj_test2), crs(ref_rast1.5))
-identical(crs(proj_test3), crs(ref_rast3))
+# Select and average summer rain (June, July, August)
+sum_prec_1500 <- sum(r_prec_1500$USA_wc2.1_30s_prec_6 + r_prec_1500$USA_wc2.1_30s_prec_7 + r_prec_1500$USA_wc2.1_30s_prec_8)/3
+win_prec_1500 <- sum(r_prec_1500$USA_wc2.1_30s_prec_12 + r_prec_1500$USA_wc2.1_30s_prec_1 + r_prec_1500$USA_wc2.1_30s_prec_2)/3
+
+# Write rasters
+
